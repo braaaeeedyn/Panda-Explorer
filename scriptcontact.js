2027055030b5a36636e5a6352ac0379c35e594f5
Braedyn Thompson <braaaeeedyn@gmail.com>
2025-01-07 04:27:19 -0800

    Update scriptcontact.js

--- scriptcontact.js
+++ scriptcontact.js
@@ -1,4 +1,4 @@
-const webhookUrl = "https://discord.com/api/webhooks/1313384332840403016/M7bWyeZaZyEMY_qSLpArOYNP-h26NCWKUOEBuBGmQXu4XtUGV5F9-jbrxXBk0hypDd0q"
+const webhookUrl = "https://discord.com/api/webhooks/1326164862224699412/pcJztBW65x7ZQoVq54i8ai5QSChpxyrssHGqLFEuW7bUeNkqmw3dd5WqUhZw21HGi2e1"
 const nameEl = document.getElementById('name-input');
 const messageEl = document.getElementById('message-input');
 const sendButtonEl = document.getElementById('send-button')
